6496fa8c1c3d704bbe53b2ee86e12b6638cf21b3
Tung Duong Thanh <tung.duong.thanh@ntq-solution.com.vn>
2020-02-04 11:52:16 +0700

    Update

--- 2.springboot/4.sql_orm/src/main/java/com/example/demo/DemoApplication.java
+++ 2.springboot/4.sql_orm/src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.service.BookService;
+
 @SpringBootApplication
 public class DemoApplication  {
     
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		var context = SpringApplication.run(DemoApplication.class, args);
+		var bookService = context.getBean(BookService.class);
+		if(bookService.countBooks() == 0) {
+		    bookService.initDatabase();
+		}
+		System.out.println("Done!");
 	}
 
 }
--- 2.springboot/4.sql_orm/src/main/java/com/example/demo/service/BookService.java
+++ 2.springboot/4.sql_orm/src/main/java/com/example/demo/service/BookService.java
@@ -1,8 +1,72 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.model.Author;
+import com.example.demo.model.Book;
+import com.example.demo.model.Category;
+import com.example.demo.repository.AuthorRepository;
+import com.example.demo.repository.BookRepository;
+import com.example.demo.repository.CategoryRepository;
 
 @Service
 public class BookService {
+    
+    @Autowired private BookRepository bookRepository;
+    @Autowired private AuthorRepository authorRepository;
+    @Autowired private CategoryRepository categoryRepository;
+    
+    public long countBooks() {
+        return bookRepository.count();
+    }
+    
+    @Transactional 
+    public void addBooks() {
+        for(int i = 0; i < 10; i++) {
+            var book = new Book();
+            book.code = "B2";
+            book.name = "Book 2";
+            var auth = new Author();
+            auth.id = 3;
+            book.author = auth;
+            bookRepository.save(book);
+        }
+    }
 
+    @Transactional
+    public void initDatabase() {
+        var c1 = new Category();
+        c1.code = "C1";
+        c1.name = "Category 1";
+        categoryRepository.save(c1);
+        
+        var c2 = new Category();
+        c2.code = "C2";
+        c2.name = "Category 2";
+        categoryRepository.save(c2);
+        
+        var auth = new Author();
+        auth.code = "AUTH_001";
+        auth.name = "Author 1";
+        authorRepository.save(auth);
+        
+        var book1 = new Book();
+        book1.code = "B1";
+        book1.name = "Book 1";
+        book1.author = auth;
+        book1.categories = List.of(c1, c2);
+        bookRepository.save(book1);
+        
+        var book2 = new Book();
+        book2.code = "B2";
+        book2.name = "Book 2";
+        book2.author = auth;
+        book2.categories = List.of(c1);
+        bookRepository.save(book2);       
+        
+    }
 }
