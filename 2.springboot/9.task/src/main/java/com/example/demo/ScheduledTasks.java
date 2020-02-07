9dbde2e4d1624d150214a6a61a5dcd2015434952
Tung Duong Thanh <tung.duong.thanh@ntq-solution.com.vn>
2020-02-07 17:33:00 +0700

    Update

---
+++ 2.springboot/9.task/src/main/java/com/example/demo/ScheduledTasks.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduledTasks {
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println(System.currentTimeMillis());
+    }
+}
